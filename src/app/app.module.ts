39b97cd63389421202f66e4178c1b3f5a28e79d9
Oloo Ouma <oloosolomon@gmail.com>
2019-06-04 09:37:02 +0300

    added function in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteComponent } from './quote/quote.component';
+// import { FormComponent } from './form/form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent,
+    QuoteComponent,
+    // FormComponent,
+    QuoteDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
