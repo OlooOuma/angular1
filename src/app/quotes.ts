afb1c4159cb6a6ea70077cbda8ac46c896914f70
Oloo Ouma <oloosolomon@gmail.com>
2019-06-04 11:38:06 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,7 +0,0 @@
-export class Quotes {
-  showDescription: boolean;
-// tslint:disable-next-line: max-line-length
-  constructor(public id: number, public name: string, public author: string, public quotes: string, public completeDate: Date, public downvote: number, public upvote: number) {
-    this.showDescription = false;
-  }
-}
